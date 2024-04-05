6848a32c3fcb3a5dcff305845fb67b1b4610802c
JohnyWander <j.myslinski.pl@gmail.com>
2024-04-06 00:38:15 +0200

    MVC version added

---
+++ MVC/WebsiteCustomerChatMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebsiteCustomerChatMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
