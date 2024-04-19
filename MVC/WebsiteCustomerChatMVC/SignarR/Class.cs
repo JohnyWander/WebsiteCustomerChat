0bf90692de5573ccbc12af96b3a10987b86ec3cb
JohnyWander <j.myslinski.pl@gmail.com>
2024-04-19 20:26:21 +0200

    signalR

--- MVC/WebsiteCustomerChatMVC/SignarR/Class.cs
+++ MVC/WebsiteCustomerChatMVC/SignarR/Class.cs
@@ -1,6 +1,18 @@
-﻿namespace WebsiteCustomerChatMVC.SignarR
+﻿using Microsoft.AspNetCore.SignalR;
+using MySqlX.XDevAPI;
+// SignalR Hub
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+namespace WebsiteCustomerChatMVC.SignarR
 {
-    public class Class
+    public class ChatHub : Hub
     {
+        public async Task SendMessage(string user, string message)
+        {
+            // Broadcast message to all clients
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
     }
 }
+
+
